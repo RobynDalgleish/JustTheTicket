f4b155fd11970033473afb9f27dc2cf773893cb1
Natasha Teng <nteng3290@gmail.com>
2018-02-28 17:42:13 -0500

    OUR COMPONENTS

--- dev/scripts/components/FavouritesModal.js
+++ dev/scripts/components/FavouritesModal.js
@@ -1,1 +1,15 @@
 // Called from anywhere user is logged in 
+
+import React from 'react';
+
+class FavouritesModal extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Favourite Modal</h1>
+            </div>
+        )
+    }
+}
+
+export default FavouritesModal;
--- dev/scripts/components/SingleMovie.js
+++ dev/scripts/components/SingleMovie.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class SingleMovie extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Single Movie Page</h1>
+            </div>
+        )
+    }
+}
+
+export default SingleMovie;
