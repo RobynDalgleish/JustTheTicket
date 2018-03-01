bb12158742c61d481072094594552a39ba342b02
Carolyn McNeillie <hello@carolynmcneillie.com>
2018-03-01 11:57:59 -0500

    Got data from MoviesDB

--- dev/scripts/components/config.js
+++ dev/scripts/components/config.js
@@ -1,4 +1,6 @@
 export default {
     apiURL: 'https://api.nytimes.com/svc/movies/v2/reviews/search.json',
-    api_key: 'ad75ac6aff4d479e8ba19467693e8fd6'
+    api_key: 'ad75ac6aff4d479e8ba19467693e8fd6',
+    movieApiURL: 'https://api.themoviedb.org/3',
+    movieApiKey: 'f012df5d63927931e82fe659a8aaa3ac',
 }
